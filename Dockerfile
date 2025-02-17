4c1ce6399012f0c5e7a3f292c7598a920950d42f
Robinson Ibarra <ribarra764@gmail.com>
2025-02-16 19:52:09 -0800

    no-ref: no-ref: changes made to dockerfile and src code to create new image to push to ecr

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,8 @@ FROM node:20-alpine
 ENV MONGO_DB_USERNAME=admin \
     MONGO_DB_PWD=password
 
-RUN mkdir -p /home/app
+RUN mkdir -p /home/node-app
 
-COPY . /home/app
+COPY . /home/node-app
 
-CMD ["node", "/home/app/src/server.js"]
+CMD ["node", "/home/node-app/src/server.js"]
--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import { createServer } from "node:http";
 
 const server = createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello World!\n");
+  res.end("Hello World!!\n");
 });
 
 // starts a simple http server locally on port 3000
